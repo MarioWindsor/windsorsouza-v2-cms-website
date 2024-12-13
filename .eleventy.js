2d873e5ed7bd0f509382776a4be6301d9b816ec4
MarioWindsor <mario.windsor@gmail.com>
2024-12-13 21:51:17 +0530

    Front-end: Home Page: Responsive Project Cards

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("css"); // Copy `css/` to `_site/css`
+	eleventyConfig.addPassthroughCopy("media"); // Copy `media/` to `_site/media`
+	eleventyConfig.addPassthroughCopy("admin"); // Copy `admin/` to `_site/admin`
+	eleventyConfig.addPassthroughCopy("project"); // Copy `admin/` to `_site/project`
+	eleventyConfig.addPassthroughCopy("js"); // Copy `JS/` to `_site/JS`
+	eleventyConfig.addPassthroughCopy("sitemap.xml");
+	eleventyConfig.addPassthroughCopy("robots.txt");
+
+	eleventyConfig.addFilter("postDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+	});
+
+};
